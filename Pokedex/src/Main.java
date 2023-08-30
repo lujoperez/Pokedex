3aa9ef842039af0cfed335ec0d94327d3e707c70
Elisantib <elisa.santibaez1994@gmail.com>
2023-08-30 16:35:33 -0400

    Prueba push1 de eli

--- Pokedex/src/Main.java
+++ Pokedex/src/Main.java
@@ -1,4 +1,4 @@
-package Pokedex.src;
+import Pokedex.src.Pokemon;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         Pokemon Pikachu = new Pokemon("Pikachu", 25, "Electrico", null);
         System.out.println(Pikachu);
+        System.out.println("Esta la caga");
     }
 }
